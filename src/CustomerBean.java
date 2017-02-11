c1058a400b2a3488698a9b1bdcb8275abaf0cc55
xiaofeng1986 <chnathena@gmail.com>
2017-02-11 17:22:44 +0800

    Enhance the CSV generation and update value
    1.Add CustomerBean to handle many CSV field 
    2.Enhance write method could handle any lines writeWithCsvBeanWriter(int
    number)
    3. Add two method related with update
    
    updateWithCsvBeanReader()
    
    writeWithCsvBeanWriterWithBean(List<CustomerBean> customersList)

---
+++ src/CustomerBean.java
@@ -0,0 +1,525 @@
+
+public class CustomerBean {
+
+	
+	private String status;
+	private String userid;
+	private String username;
+	private String firstname;
+	private String lastname;
+	private String mi;
+	private String gender;
+	private String email;
+	private String manager;
+	private String hr;
+	private String department;
+	private String jobcode;
+	private String division;
+	private String location;
+	private String timezone;
+	private String hiredate;
+	private String empid;
+	private String title;
+	private String biz_phone;
+	private String fax;
+	private String addr1;
+	private String addr2;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	private String review_freq;
+	private String last_review_date;
+	private String custom01;
+	private String custom02;
+	private String custom03;
+	private String custom04;
+	private String custom05;
+	private String custom06;
+	private String custom07;
+	private String custom08;
+	private String custom09;
+	private String custom10;
+	private String custom11;
+	private String custom12;
+	private String custom13;
+	private String custom14;
+	private String custom15;
+	private String matrix_manager;
+	private String default_locale;
+	private String proxy;
+	private String custom_manager;
+	private String second_manager;
+	
+	public CustomerBean(){
+	}
+	
+	public CustomerBean(final String status, final String userid, final String username
+			, final String firstname, final String lastname, final String mi, final String gender
+			, final String email, final String manager, final String hr, final String department
+			, final String jobcode, final String division, final String location, final String timezone
+			, final String hiredate, final String empid, final String title, final String biz_phone
+			, final String fax, final String addr1, final String addr2, final String city
+			, final String state, final String zip, final String country, final String review_freq
+			, final String last_review_date, final String custom01, final String custom02
+			, final String custom03, final String custom04, final String custom05, final String custom06
+			, final String custom07, final String custom08, final String custom09, final String custom10
+			, final String custom11, final String custom12, final String custom13, final String custom14
+			, final String custom15, final String matrix_manager, final String default_locale
+			,final String proxy, final String custom_manager, final String second_manager) {
+		this.status = status;
+		this.userid = userid;
+		this.username = username;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.mi = mi;
+		this.gender = gender;
+		this.email = email;
+		this.manager = manager;
+		this.hr = hr;
+		this.department = department;
+		this.jobcode = jobcode;
+		this.division = division;
+		this.location = location;
+		this.timezone = timezone;
+		this.hiredate = hiredate;
+		this.empid = empid;
+		this.title = title;
+		this.biz_phone = biz_phone;
+		this.fax = fax;
+		this.addr1 = addr1;
+		this.addr2 = addr2;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.country = country;
+		this.review_freq = review_freq;
+		this.last_review_date = last_review_date;
+		this.custom01 = custom01;
+		this.custom02 = custom02;
+		this.custom03 = custom03;
+		this.custom04 = custom04;
+		this.custom05 = custom05;
+		this.custom06 = custom06;
+		this.custom07 = custom07;
+		this.custom08 = custom08;
+		this.custom09 = custom09;
+		this.custom10 = custom10;
+		this.custom11 = custom11;
+		this.custom12 = custom12;
+		this.custom13 = custom13;
+		this.custom14 = custom14;
+		this.custom15 = custom15;
+		this.matrix_manager = matrix_manager;
+		this.default_locale = default_locale;
+		this.proxy = proxy;
+		this.custom_manager = custom_manager;
+		this.second_manager = second_manager;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMi() {
+		return mi;
+	}
+
+	public void setMi(String mi) {
+		this.mi = mi;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getManager() {
+		return manager;
+	}
+
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+
+	public String getHr() {
+		return hr;
+	}
+
+	public void setHr(String hr) {
+		this.hr = hr;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getJobcode() {
+		return jobcode;
+	}
+
+	public void setJobcode(String jobcode) {
+		this.jobcode = jobcode;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public String getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBiz_phone() {
+		return biz_phone;
+	}
+
+	public void setBiz_phone(String biz_phone) {
+		this.biz_phone = biz_phone;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getAddr1() {
+		return addr1;
+	}
+
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+
+	public String getAddr2() {
+		return addr2;
+	}
+
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getReview_freq() {
+		return review_freq;
+	}
+
+	public void setReview_freq(String review_freq) {
+		this.review_freq = review_freq;
+	}
+
+	public String getLast_review_date() {
+		return last_review_date;
+	}
+
+	public void setLast_review_date(String last_review_date) {
+		this.last_review_date = last_review_date;
+	}
+
+	public String getCustom01() {
+		return custom01;
+	}
+
+	public void setCustom01(String custom01) {
+		this.custom01 = custom01;
+	}
+
+	public String getCustom02() {
+		return custom02;
+	}
+
+	public void setCustom02(String custom02) {
+		this.custom02 = custom02;
+	}
+
+	public String getCustom03() {
+		return custom03;
+	}
+
+	public void setCustom03(String custom03) {
+		this.custom03 = custom03;
+	}
+
+	public String getCustom04() {
+		return custom04;
+	}
+
+	public void setCustom04(String custom04) {
+		this.custom04 = custom04;
+	}
+
+	public String getCustom05() {
+		return custom05;
+	}
+
+	public void setCustom05(String custom05) {
+		this.custom05 = custom05;
+	}
+
+	public String getCustom06() {
+		return custom06;
+	}
+
+	public void setCustom06(String custom06) {
+		this.custom06 = custom06;
+	}
+
+	public String getCustom07() {
+		return custom07;
+	}
+
+	public void setCustom07(String custom07) {
+		this.custom07 = custom07;
+	}
+
+	public String getCustom08() {
+		return custom08;
+	}
+
+	public void setCustom08(String custom08) {
+		this.custom08 = custom08;
+	}
+
+	public String getCustom09() {
+		return custom09;
+	}
+
+	public void setCustom09(String custom09) {
+		this.custom09 = custom09;
+	}
+
+	public String getCustom10() {
+		return custom10;
+	}
+
+	public void setCustom10(String custom10) {
+		this.custom10 = custom10;
+	}
+
+	public String getCustom11() {
+		return custom11;
+	}
+
+	public void setCustom11(String custom11) {
+		this.custom11 = custom11;
+	}
+
+	public String getCustom12() {
+		return custom12;
+	}
+
+	public void setCustom12(String custom12) {
+		this.custom12 = custom12;
+	}
+
+	public String getCustom13() {
+		return custom13;
+	}
+
+	public void setCustom13(String custom13) {
+		this.custom13 = custom13;
+	}
+
+	public String getCustom14() {
+		return custom14;
+	}
+
+	public void setCustom14(String custom14) {
+		this.custom14 = custom14;
+	}
+
+	public String getCustom15() {
+		return custom15;
+	}
+
+	public void setCustom15(String custom15) {
+		this.custom15 = custom15;
+	}
+
+	public String getMatrix_manager() {
+		return matrix_manager;
+	}
+
+	public void setMatrix_manager(String matrix_manager) {
+		this.matrix_manager = matrix_manager;
+	}
+
+	public String getDefault_locale() {
+		return default_locale;
+	}
+
+	public void setDefault_locale(String default_locale) {
+		this.default_locale = default_locale;
+	}
+
+	public String getProxy() {
+		return proxy;
+	}
+
+	public void setProxy(String proxy) {
+		this.proxy = proxy;
+	}
+
+	public String getCustom_manager() {
+		return custom_manager;
+	}
+
+	public void setCustom_manager(String custom_manager) {
+		this.custom_manager = custom_manager;
+	}
+
+	public String getSecond_manager() {
+		return second_manager;
+	}
+
+	public void setSecond_manager(String second_manager) {
+		this.second_manager = second_manager;
+	}
+	
+	
+	
+	
+	public String toString() {
+		return String.format("CustomerBean [status=%s, userid=%s, username=%s, firstname=%s"
+				+ ", lastname=%s, mi=%s, gender=%s, email=%s, manager=%s, hr=%s, department=%s"
+				+ ", jobcode=%s, division=%s, location=%s, timezone=%s, hiredate=%s, empid=%s"
+				+ ", title=%s, biz_phone=%s, fax=%s, addr1=%s, addr2=%s, city=%s, state=%s"
+				+ ", zip=%s, country=%s, review_freq=%s, last_review_date=%s, custom01=%s"
+				+ ", custom02=%s, custom03=%s, custom04=%s, custom05=%s, custom06=%s"
+				+ ", custom07=%s, custom08=%s, custom09=%s, custom10=%s, custom11=%s"
+				+ ", custom12=%s, custom13=%s, custom14=%s, custom15=%s, matrix_manager=%s"
+				+ ", default_locale=%s, proxy=%s, custom_manager=%s, second_manager=%s]", 
+				status, userid, username, firstname, lastname, mi, gender, email, manager, hr
+				, department, jobcode, division, location, timezone, hiredate, empid, title
+				, biz_phone, fax, addr1, addr2, city, state, zip, country, review_freq
+				, last_review_date, custom01, custom02, custom03, custom04, custom05, custom06
+				, custom07, custom08, custom09, custom10, custom11, custom12, custom13, custom14
+				, custom15, matrix_manager, default_locale, proxy, custom_manager, second_manager);
+	}
+
+}
--- src/superCSVTesting.java
+++ src/superCSVTesting.java
@@ -3,38 +3,262 @@
  */
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.supercsv.cellprocessor.constraint.NotNull;
 import org.supercsv.cellprocessor.constraint.UniqueHashCode;
 import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.CsvListReader;
 import org.supercsv.io.CsvListWriter;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.io.ICsvListReader;
 import org.supercsv.io.ICsvListWriter;
 import org.supercsv.prefs.CsvPreference;
 
 
 public class superCSVTesting {
-   private static final String CSV_FILENAME = "/Users/mac/Downloads/writeWithCsvListWritet.csv";
+   private static final String CSV_FILENAME = "/Users/mac/Downloads/writeWithCsvBeanWriter.csv";
     
    private static CellProcessor[] getProcessors(){
 	   
 	   final CellProcessor[] processors = new CellProcessor[] {
-	            new UniqueHashCode(),//number
-	            new NotNull()//name
+			   new NotNull(),
+			   new UniqueHashCode(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull(),
+	           new NotNull()
 	    };
 	   
 	   return processors;
    }
     
-    
+    private static void writeWithCsvBeanWriter(int number) throws Exception {
+    	
+//    	List<CustomerBean> customers = Arrays.asList(john);
+//    	System.out.println(customers.size());
+//    	CustomerBean john = new CustomerBean ("1","john1");
+//      CustomerBean john2 = new CustomerBean ("2","john2");
+//    	customers.add(john);
+//    	customers.add(john2);
+    	
+    	List<CustomerBean> customers = new ArrayList<CustomerBean>();    	
+    	for ( int i = 0 ; i < number ; i ++) {
+    		String no = Integer.toString(i);
+    		CustomerBean user = new CustomerBean("active","1705SuperCSV"+no,"1705SuperCSV"+no
+    				,"1705xiaofeng","ge","mi","F","xiaofeng.ge@sap.com","NO_MANAGER","NO_HR"
+    				,"dep01","job01","DI01","LO01","US/Alaska","2017-01-01","empid01","title01"
+    				,"phone01","fax01","addr1","addr2","NY","NY","123456","USA","review","2017-01-01"
+    				,"CF1_001","CF2_001","CF3_001","CF4_001","CF5_001","CF6_001","CF7_001","CF8_001"
+    				,"CF9_001","CF10_001","CF11_001","CF12_001","CF13_001","CF14_001","CF15_001"
+    				,"NO_MANAGER","en_US","proxy","NO_MANAGER","NO_MANAGER");
+    		customers.add(user);
+    	}
    
+    	
+    	
+    	
+    	ICsvBeanWriter beanWriter = null;
+    	try {
+    		beanWriter = new CsvBeanWriter(
+    				new FileWriter("/Users/mac/Downloads/writeWithCsvBeanWriter.csv")
+    				,CsvPreference.STANDARD_PREFERENCE);
+    		final String[] header = new String[]{"STATUS","USERID","USERNAME","FIRSTNAME","LASTNAME"
+    				,"MI","GENDER","EMAIL","MANAGER","HR","DEPARTMENT","JOBCODE","DIVISION"
+    				,"LOCATION","TIMEZONE","HIREDATE","EMPID","TITLE","BIZ_PHONE","FAX","ADDR1"
+    				,"ADDR2","CITY","STATE","ZIP","COUNTRY","REVIEW_FREQ","LAST_REVIEW_DATE"
+    				,"CUSTOM01","CUSTOM02","CUSTOM03","CUSTOM04","CUSTOM05","CUSTOM06","CUSTOM07"
+    				,"CUSTOM08","CUSTOM09","CUSTOM10","CUSTOM11","CUSTOM12","CUSTOM13","CUSTOM14"
+    				,"CUSTOM15","MATRIX_MANAGER","DEFAULT_LOCALE","PROXY","CUSTOM_MANAGER","SECOND_MANAGER"};
+    		final String[] headername = new String[] {"STATUS","USERID","Username","First Name","Last Name" 
+    		,"Middle Name","Gender","Email","Manager","Human Resource","Department","Job Code","Division"
+    		,"Location","Time Zone","Hire Date","Employee Id","Title","Business Phone","Business Fax"
+    		,"Address 1","Address 2","City","State","ZIP","Country","Review Frequency","Last Review Date"
+    		,"Customizable Field 1","Customizable Field 2","Customizable Field 3","Customizable Field 4"
+    		,"Customizable Field 5","Customizable Field 6","Customizable Field 7","Customizable Field 8"
+    		,"Customizable Field 9","Customizable Field 10","Customizable Field 11","Customizable Field 12"
+    		,"Customizable Field 13","Customizable Field 14","Customizable Field 15","Matrix Manager"
+    		,"Default Locale","Proxy","Custom Manager","Second Manager"};
+    		final CellProcessor[] processors = getProcessors();
+    	
+    		beanWriter.writeHeader(header);
+    		beanWriter.writeHeader(headername);
+    		
+    		final String[] headerLowercase = new String[]{"status", "userid", "username", "firstname"
+    				, "lastname", "mi", "gender", "email", "manager", "hr", "department", "jobcode"
+    				, "division", "location", "timezone", "hiredate", "empid", "title", "biz_phone"
+    				, "fax", "addr1", "addr2", "city", "state", "zip", "country", "review_freq"
+    				, "last_review_date", "custom01", "custom02", "custom03", "custom04", "custom05"
+    				, "custom06", "custom07", "custom08", "custom09", "custom10", "custom11"
+    				, "custom12", "custom13", "custom14", "custom15", "matrix_manager"
+    				, "default_locale", "proxy", "custom_manager", "second_manager"};
+    		
+    		for (final CustomerBean customer : customers) {
+    			beanWriter.write(customer, headerLowercase, processors);
+    		}
+    	}
+    	finally {
+    		if(beanWriter != null){
+    			beanWriter.close();
+    		}
+    	}
+    	
+    }
     
-//    private static void writeWithCsvBeanWriter() throws Exception {
-//    	final CustomerBean john = new CustomerBean ("1","John");
-//    }
+private static void writeWithCsvBeanWriterWithBean(List<CustomerBean> customersList) throws Exception {
+    	
+    	ICsvBeanWriter beanWriter = null;
+    	try {
+    		beanWriter = new CsvBeanWriter(
+    				new FileWriter("/Users/mac/Downloads/writeWithCsvBeanWriter.csv")
+    				,CsvPreference.STANDARD_PREFERENCE);
+    		final String[] header = new String[]{"STATUS","USERID","USERNAME","FIRSTNAME","LASTNAME"
+    				,"MI","GENDER","EMAIL","MANAGER","HR","DEPARTMENT","JOBCODE","DIVISION"
+    				,"LOCATION","TIMEZONE","HIREDATE","EMPID","TITLE","BIZ_PHONE","FAX","ADDR1"
+    				,"ADDR2","CITY","STATE","ZIP","COUNTRY","REVIEW_FREQ","LAST_REVIEW_DATE"
+    				,"CUSTOM01","CUSTOM02","CUSTOM03","CUSTOM04","CUSTOM05","CUSTOM06","CUSTOM07"
+    				,"CUSTOM08","CUSTOM09","CUSTOM10","CUSTOM11","CUSTOM12","CUSTOM13","CUSTOM14"
+    				,"CUSTOM15","MATRIX_MANAGER","DEFAULT_LOCALE","PROXY","CUSTOM_MANAGER","SECOND_MANAGER"};
+    		final String[] headername = new String[] {"STATUS","USERID","Username","First Name","Last Name" 
+    		,"Middle Name","Gender","Email","Manager","Human Resource","Department","Job Code","Division"
+    		,"Location","Time Zone","Hire Date","Employee Id","Title","Business Phone","Business Fax"
+    		,"Address 1","Address 2","City","State","ZIP","Country","Review Frequency","Last Review Date"
+    		,"Customizable Field 1","Customizable Field 2","Customizable Field 3","Customizable Field 4"
+    		,"Customizable Field 5","Customizable Field 6","Customizable Field 7","Customizable Field 8"
+    		,"Customizable Field 9","Customizable Field 10","Customizable Field 11","Customizable Field 12"
+    		,"Customizable Field 13","Customizable Field 14","Customizable Field 15","Matrix Manager"
+    		,"Default Locale","Proxy","Custom Manager","Second Manager"};
+    		final CellProcessor[] processors = getProcessors();
+    	
+    		beanWriter.writeHeader(header);
+    	//	beanWriter.writeHeader(headername);
+    		
+    		final String[] headerLowercase = new String[]{"status", "userid", "username", "firstname"
+    				, "lastname", "mi", "gender", "email", "manager", "hr", "department", "jobcode"
+    				, "division", "location", "timezone", "hiredate", "empid", "title", "biz_phone"
+    				, "fax", "addr1", "addr2", "city", "state", "zip", "country", "review_freq"
+    				, "last_review_date", "custom01", "custom02", "custom03", "custom04", "custom05"
+    				, "custom06", "custom07", "custom08", "custom09", "custom10", "custom11"
+    				, "custom12", "custom13", "custom14", "custom15", "matrix_manager"
+    				, "default_locale", "proxy", "custom_manager", "second_manager"};
+    		
+    		for (final CustomerBean customer : customersList) {
+    			beanWriter.write(customer, headerLowercase, processors);
+    		}
+    	}
+    	finally {
+    		if(beanWriter != null){
+    			beanWriter.close();
+    		}
+    	}
+    	
+    }
+    
+    
+    private static void readWithCsvBeanReader() throws Exception {
+    	
+    	ICsvBeanReader beanReader = null;
+    	try {
+    		beanReader = new CsvBeanReader(new FileReader(CSV_FILENAME)
+    				,CsvPreference.STANDARD_PREFERENCE);
+    		
+    		final String[] header = beanReader.getHeader(false);
+    		final CellProcessor [] processors = getProcessors();
+    		
+    		CustomerBean customer;
+    		while(( customer = beanReader.read(CustomerBean.class, header, processors)) != null ) {
+    			System.out.println(String.format("lineNo=%s, rowNo=%s, customer=%s"
+    					, beanReader.getLineNumber(),beanReader.getRowNumber(),customer));
+    				
+    		}
+    	}
+    	finally {
+    		if (beanReader != null ){
+    			beanReader.close();
+    		}
+    	}
+    }
+    
+    
+private static void updateWithCsvBeanReader() throws Exception {
+    	
+    	ICsvBeanReader beanReader = null;
+    	List<CustomerBean> customers = new ArrayList<CustomerBean>();
+    	try {
+    		beanReader = new CsvBeanReader(new FileReader(CSV_FILENAME)
+    				,CsvPreference.STANDARD_PREFERENCE);
+    		
+    		final String[] header = beanReader.getHeader(false);
+    		final CellProcessor [] processors = getProcessors();
+    		
+    		CustomerBean customer;
+    		 
+    		while(( customer = beanReader.read(CustomerBean.class, header, processors)) != null ) {
+    			if (beanReader.getRowNumber() == 5) {
+    				String name = customer.getFirstname();
+    				customer.setFirstname(name+"03");
+    			}
+    			customers.add(customer);		
+    		}
+  
+        		
+    	}
+    	finally {
+    		if (beanReader != null ){
+    			beanReader.close();
+    		}
+    	}
+    	
+    	writeWithCsvBeanWriterWithBean(customers);
+    
+    }
     
     private static void writeWithCsvListWriter() throws Exception {
     	final List<Object> john = Arrays.asList(new Object[]{"1","john"});
@@ -85,9 +309,17 @@ public class superCSVTesting {
     }
     
     public static void main(String[] args) throws Exception {
-    	//System.out.print("Hello world");
-    	//the first testing 
-    	writeWithCsvListWriter();
-    	readWithCsvListReader();
+//    	writeWithCsvListWriter();
+//    	readWithCsvListReader();
+    	
+
+    	writeWithCsvBeanWriter(10);
+    	readWithCsvBeanReader();
+    	System.out.println("=====================updated===============");
+    	updateWithCsvBeanReader();
+    	readWithCsvBeanReader();
+    	
+
+    	
     }
 }
