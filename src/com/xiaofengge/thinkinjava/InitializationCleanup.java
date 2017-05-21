85dd406ea36d77a78ab087a47ce4a056f0ce50bf
xiaofeng1986 <chnathena@gmail.com>
2017-05-21 16:41:41 +0800

    add random seed 47

--- src/com/xiaofengge/thinkinjava/InitializationCleanup.java
+++ src/com/xiaofengge/thinkinjava/InitializationCleanup.java
@@ -1,5 +1,9 @@
 package com.xiaofengge.thinkinjava;
 
+import java.util.Arrays;
+import java.util.Random;
+import static net.mindview.util.Print.*;
+
 public class InitializationCleanup {
 	
 	boolean checkedOut = false;
@@ -11,19 +15,28 @@ public class InitializationCleanup {
 		checkedOut = false ;
 	}
 	
-	protected void  finalize () {
-		if (checkedOut) {
-			System.out.print("Error : checked out");
-		}
-	}
+//	protected void  finalize () {
+//		if (checkedOut) {
+//			System.out.print("Error : checked out");
+//		}
+//	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		InitializationCleanup novel = new InitializationCleanup(true);
-		novel.checkIn();
-		new InitializationCleanup(true);
-		System.gc();
+//		InitializationCleanup novel = new InitializationCleanup(true);
+//		novel.checkIn();
+//		new InitializationCleanup(true);
+//		System.gc();
+		
+		int[] a;
+		Random rand = new Random(47);
+		int ran = rand.nextInt(10);
+		print(ran);
+		a = new int[ran];
+		print("length of a = " + a.length);
+		print(Arrays.toString(a));
+		
 	}
 
 }
